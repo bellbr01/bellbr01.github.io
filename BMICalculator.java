c15bb77312a4e68933c48e2ec0440798450a99fa
bellbr01 <bellbr01@luther.edu>
2015-03-08 20:44:43 -0500

    Update BMICalculator.java

--- BMICalculator.java
+++ BMICalculator.java
@@ -1,1 +1,21 @@
+<html>
+<head>
+<h1>Temperature Converter </h1>
+</head>
+<body>
+<label for="box"> Temperature in Degrees Celsius </label>
+<input type="text" id="box">
+<button onclick="Button();">Click To Convert </button>
+<h2>id="output></h2>
 
+<script>
+button=function() {
+  box=document.querySelector("#box");
+  temperature=thebox.value;
+  theh2=document.querySelector("#output");
+  theh2.innerHTML = temperature * 1.8 + 32 + "Degrees in Fahrenheit"
+}
+
+</script>
+</body>
+</html>
